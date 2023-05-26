39598efd7a8aa3e2d9549ead50152d49fddce2a6
Walker de Alencar <walkeralencar@gmail.com>
2023-05-26 17:53:07 -0300

    retrieve tags and show version

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 # SEMVER - Semantic Versioning
+
+Shell script for SEMVER to be used on CI/CD.
+
+1. It retrieves the latest tag from the repository using `git describe`.
---
+++ semver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get the latest tag
+latest_tag=$(git describe --tags --abbrev=0)
+
+# Parse the major, minor, and patch versions from the latest tag
+IFS='.' read -r major minor patch <<< "${latest_tag//v}"
+
+# Construct the new version
+new_version="v${major}.${minor}.${patch}"
+
+echo "New version: $new_version"
