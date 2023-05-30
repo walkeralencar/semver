5145473d125f6a2656c0a8cc3d3a3d9545e6b0a4
Walker de Alencar <walkeralencar@gmail.com>
2023-05-30 18:44:26 -0300

    verify if tag exists in current commit

--- semver.sh
+++ semver.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Get tag at the current commit
+latest_semver_tag=$(git describe --tags --match "[0-9]*.[0-9]*.[0-9]*" --abbrev=0 --contains HEAD > /dev/null 2>&1)
+
+# If tag already exists, exit
+if [ -n "$latest_semver_tag" ]; then
+    echo "A SemVer tag already exists: $latest_semver_tag"
+    exit 0
+fi
+
 # Get the latest tag
 latest_tag=$(git describe --tags --abbrev=0)
 
