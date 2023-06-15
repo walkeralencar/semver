25ddad373c2e38631208bd7aabf99ec34465b3ea
Walker de Alencar <walkeralencar@gmail.com>
2023-06-15 17:10:13 -0300

    Extract the branch name from the commit message

--- semver.sh
+++ semver.sh
@@ -18,6 +18,12 @@ IFS='.' read -r major minor patch <<< "${latest_tag//v}"
 # Get message of the current commit
 commit_message=$(git log --format=%B -n 1 HEAD)
 
+# Extract the branch name from the commit message
+branch=""
+if [[ $commit_message =~ ^Merge\ branch\ \'(release\/|feature\/|hotfix\/)([^\']+)\' ]]; then
+    branch=${BASH_REMATCH[1]}
+fi
+
 # Construct the new version
 new_version="${major}.${minor}.${patch}"
 
